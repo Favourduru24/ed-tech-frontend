07b48d2f92d859e46985e91192d6e6f7171c2156
Duru Favour <favourduru24@gmail.com>
2026-01-01 01:56:31 -0800

    added credential to my request

--- app/api/axios.js
+++ app/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
 export const imageCofig = axios.create({
-     baseURL: 'https://ed-tech-backend-62u0dp1h6-starks-projects-f3f5ee78.vercel.app/'
+     baseURL: 'https://ed-tech-backend-62u0dp1h6-starks-projects-f3f5ee78.vercel.app/',
+     withCredentials: true
 })

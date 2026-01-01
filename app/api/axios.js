c66535fdcf5e535f5b9ed9b5f16ceb800f6b05eb
Duru Favour <favourduru24@gmail.com>
2026-01-01 02:21:22 -0800

    new code update redeploying

--- app/api/axios.js
+++ app/api/axios.js
@@ -2,5 +2,4 @@ import axios from "axios"
 
 export const imageCofig = axios.create({
      baseURL: 'https://ed-tech-backend-62u0dp1h6-starks-projects-f3f5ee78.vercel.app/',
-     withCredentials: true
 })
